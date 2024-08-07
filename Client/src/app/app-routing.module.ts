1006e0c62a37367dc433a72deaca78b15f9d3233
Vedant Kasliwal <43548608+vedant10@users.noreply.github.com>
2024-08-07 19:28:39 +0530

    Add files via upload

---
+++ Client/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
+  { path: 'tasks', component: TasksListComponent },
+  { path: 'add', component: AddTaskComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { TasksListComponent } from './components/tasks-list/tasks-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddTaskComponent,
+    TasksListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
